fa372e20b9c1b2ff14202b47eba819c2ed5e4b4f
Solar Designer <solar@owl.openwall.com>
2006-03-05 10:41:40 +0000

    1.0.1

--- popa3d/version.c
+++ popa3d/version.c
@@ -2,6 +2,5 @@
  * popa3d version information.
  */
 
-char popa3d_version[] = "1.0";
-char popa3d_date[] = "$Date: 2004/07/22 18:52:16 $";
-char popa3d_id[] = "@(#) $Id: version.c,v 1.4 2004/07/22 18:52:16 solar Exp $";
+char popa3d_version[] = "1.0.1";
+char popa3d_date[] = "2006/03/05";
